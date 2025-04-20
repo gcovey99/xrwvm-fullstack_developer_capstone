99f000b09bc48ef2b3ba8b8dcbf3fea46ff68f13
gcovey99 <geoffcovey@gmail.com>
2025-04-19 23:55:52 -0400

    Your commit message here

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,21 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import React from 'react';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
-  );
-}
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/register" component={Register} />
+            {/* Define other routes here */}
+            <Route path="/" exact>
+              <h1>Welcome to the Home Page</h1>
+              {/* Add home page content or component */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 export default App;
