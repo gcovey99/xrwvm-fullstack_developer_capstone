b0ffff2beb6729abbd5d4017f12c84b9f8d07c13
gcovey99 <geoffcovey@gmail.com>
2025-04-27 21:23:57 -0400

    Update changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,9 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import React from 'react';
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
     return (
@@ -9,6 +12,9 @@ function App() {
           <Switch>
             <Route path="/register" component={Register} />
             {/* Define other routes here */}
+            <Route path="/dealers" element={<Dealers/>} />
+            <Route path="/dealer/:id" element={<Dealer/>} />
+            <Route path="/postreview/:id" element={<PostReview/>} />
             <Route path="/" exact>
               <h1>Welcome to the Home Page</h1>
               {/* Add home page content or component */}
